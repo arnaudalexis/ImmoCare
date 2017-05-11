cc5d3e56e28a79696b5fdac8463c5c20a4fda531
kevin.nguyen <kevin.nguyen@saaswedo.com>
2017-05-11 17:03:30 +0200

    update login view

--- ImmoCare/LoginViewController.swift
+++ ImmoCare/LoginViewController.swift
@@ -6,8 +6,6 @@
 //  Copyright © 2017 ImmoCare. All rights reserved.
 //
 
-//TEST
-
 import UIKit
 
 class LoginViewController: UIViewController {
@@ -20,23 +18,39 @@ class LoginViewController: UIViewController {
     }()
     
     // email
-    var email: MyTextField = {
+    lazy var email: MyTextField = {
         let lemail = MyTextField(frame:CGRect(x:10, y:100, width:280, height:40))
         lemail.backgroundColor = .white
         lemail.placeholder = "Email"
         lemail.keyboardType = .emailAddress
         lemail.font = UIFont.systemFont(ofSize:14)
+        lemail.leftViewMode = .always
+        lemail.leftView = self.getLeftView(image: #imageLiteral(resourceName: "usernameIcon"))
         return lemail
     }()
 
     // password
-    var password: MyTextField = {
+    lazy var password: MyTextField = {
         let pwd = MyTextField(frame:CGRect(x:10, y:160, width:280, height:40))
         pwd.backgroundColor = .white
         pwd.placeholder = "Password"
         pwd.font = UIFont.systemFont(ofSize:14)
+        pwd.leftViewMode = .always
+        pwd.leftView = self.getLeftView(image: #imageLiteral(resourceName: "pwdIcon"))
         return pwd
     }()
+    
+    // set left container for pwd and username 
+    func getLeftView(image:UIImage) -> UIView
+    {
+        let leftV = UIView(frame:CGRect(x:0, y:0, width:40, height:40))
+        let img = UIImageView(frame:CGRect(x:5, y:5, width:30, height:30))
+        img.image = image
+        leftV.backgroundColor = UIColor(red:0.27, green:0.83, blue:0.76, alpha: 1.0)
+        leftV.addSubview(img)
+        return leftV
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -68,6 +82,21 @@ class LoginViewController: UIViewController {
         btnLogin.addTarget(self, action: #selector(loginAction), for: .touchUpInside)
         self.container.addSubview(btnLogin)
         
+        // set btn forgot password
+        let btnForgotPwd = UIButton(frame:CGRect(x:10, y:260, width:150, height:40))
+        btnForgotPwd.setTitle("Forget Password", for: .normal)
+        btnForgotPwd.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        btnForgotPwd.setTitleColor(UIColor(red:0.98, green:0.98, blue:0.98, alpha:1.0), for: .normal)
+        self.container.addSubview(btnForgotPwd)
+        
+        // set btn register
+        let btnRegister = UIButton(frame:CGRect(x:210, y:260, width:90, height:40))
+        btnRegister.setTitle("Register", for: .normal)
+        btnRegister.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        btnRegister.setTitleColor(UIColor(red:0.98, green:0.98, blue:0.98, alpha:1.0), for: .normal)
+        btnRegister.addTarget(self, action: #selector(registerAction), for: .touchUpInside)
+        self.container.addSubview(btnRegister)
+        
         // set position container on center
         self.container.center = self.view.center
         
@@ -78,7 +107,14 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
     func loginAction()
+    {
+        
+    }
+    
+    func registerAction()
     {
         
     }
